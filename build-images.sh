b20b9a2ca6cf16736bf2334b9c673d5ae0336d35
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 13:01:52 +0000

    Improve log format for buildx

--- build-images.sh
+++ build-images.sh
@@ -7,6 +7,6 @@ docker rm -f buildx_buildkit_docker-multiarch0
 docker context create cimg-ma
 docker buildx create cimg-ma --name docker-multiarch --driver docker-container --platform linux/amd64,linux/arm64 --use
 docker buildx inspect --builder docker-multiarch --bootstrap
-docker buildx build --push --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
-docker buildx build --push --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
-docker buildx build --push --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
+docker buildx build --push --progress plain --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
+docker buildx build --push --progress plain --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
+docker buildx build --push --progress plain --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
