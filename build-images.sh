a082ea96caf4d6288f91ad3486acc8f18d539ed5
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 02:05:11 +0000

    Use buildx for multiarch images

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 docker buildx install
-docker build --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
-docker build --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
-docker build --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
+docker buildx create --name docker-multiarch --platform linux/amd64,linux/arm64
+docker buildx inspect --builder docker-multiarch --bootstrap
+docker buildx use docker-multiarch
+docker buildx build --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
+docker buildx build --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
+docker buildx build --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
