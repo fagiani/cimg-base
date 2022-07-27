0d05d8e6b48d58cd3b6299e4316f8bfd89351f9d
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 03:14:52 +0000

    Fix typo

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker buildx install
-docker run --rm privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker rm -f buildx_buildkit_docker-multiarch0
 docker context create cimg-ma
 docker buildx create cimg-ma --name docker-multiarch --platform linux/amd64,linux/arm64 --use
