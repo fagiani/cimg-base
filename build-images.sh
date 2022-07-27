519848de7eebe938026c8695009860613e8f7862
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 03:01:35 +0000

    Try using qemu-user-static before custom builder

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 docker buildx install
+docker run --rm privileged multiarch/qemu-user-static --reset -p yes
+docker rm -f buildx_buildkit_docker-multiarch0
 docker context create cimg-ma
 docker buildx create cimg-ma --name docker-multiarch --platform linux/amd64,linux/arm64 --use
 docker buildx inspect --builder docker-multiarch --bootstrap
