6c00510daa24611d2575a01783639f75b5c6833e
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 02:09:04 +0000

    Create docker context for buildx

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker buildx install
-docker buildx create --name docker-multiarch --platform linux/amd64,linux/arm64
+docker context create cimg-ma
+docker buildx create cimg-ma --name docker-multiarch --platform linux/amd64,linux/arm64 --use
 docker buildx inspect --builder docker-multiarch --bootstrap
-docker buildx use docker-multiarch
 docker buildx build --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
 docker buildx build --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
 docker buildx build --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
