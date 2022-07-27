8d901eac62f589e4b8ea3b2dc5ca0e928f4a2a08
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 12:52:45 +0000

    Add docker push to multiarch as load won't work

--- build-images.sh
+++ build-images.sh
@@ -5,8 +5,8 @@ docker buildx install
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker rm -f buildx_buildkit_docker-multiarch0
 docker context create cimg-ma
-docker buildx create cimg-ma --name docker-multiarch --platform linux/amd64,linux/arm64 --use
+docker buildx create cimg-ma --name docker-multiarch --driver docker-container --platform linux/amd64,linux/arm64 --use
 docker buildx inspect --builder docker-multiarch --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
-docker buildx build --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
-docker buildx build --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
