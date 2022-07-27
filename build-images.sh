96c98770632cff68e820a3129d6869eef484d724
Paulo Fagiani <pfagiani@gmail.com>
2022-07-27 01:52:28 +0000

    Initial multiarch image changes

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 18.04/Dockerfile -t cimg/base:18.04 .
-docker build --file 20.04/Dockerfile -t cimg/base:20.04 .
-docker build --file 22.04/Dockerfile -t cimg/base:22.04 .
+docker buildx install
+docker build --platform linux/amd64,linux/arm64 --file 18.04/Dockerfile -t fagiani/cimg-base:18.04 .
+docker build --platform linux/amd64,linux/arm64 --file 20.04/Dockerfile -t fagiani/cimg-base:20.04 .
+docker build --platform linux/amd64,linux/arm64 --file 22.04/Dockerfile -t fagiani/cimg-base:22.04 .
